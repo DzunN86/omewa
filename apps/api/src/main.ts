b1915fb039c2ee8d39f052807c33a82e2f28c68c
Dzun <nurroinz@gmail.com>
2022-09-13 19:23:29 +0700

    go deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -19,7 +19,7 @@ app.get('/api', (req, res) => {
 BlogRoutes(app);
 AuthRoutes(app);
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
